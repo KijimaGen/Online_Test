2b4900368431bc83dc0ab29d9b7e2c61aef0cc9d
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-04-16 15:23:53 +0900

    SceneButton

--- Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
+++ Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
@@ -1,26 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TitleButton : MonoBehaviour
 {
-    [SerializeField] Button focusButton;
+    Button GameStart;
+    Button Rule;
+    Button Exit;
 
     // Start is called before the first frame update
     void Start()
     {
         // ボタンコンポーネントの取得
-        focusButton = focusButton.GetComponent<Button>();
+        GameStart = GameObject.Find("/Canvas/GameStart").GetComponent<Button>();
+        Rule = GameObject.Find("/Canvas/Rule").GetComponent<Button>();
+        Exit = GameObject.Find("/Canvas/Exit").GetComponent<Button>();
+        GameStart.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void OnClickStart() {
         // 全てのフォーカスを解除
         EventSystem.current.SetSelectedGameObject(null);
-        // focusBottonにフォーカスする
-        focusButton.Select();
+
+        // ゲームシーン
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnClickRule() {
+        // 全てのフォーカスを解除
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void OnClickExit() {
+        // 全てのフォーカスを解除
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // ゲームを終了する
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 }
