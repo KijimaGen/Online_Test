b32738d01e1c8ac1bda540bd69b237b8b412de0e
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-04-18 15:11:30 +0900

    Rule

--- Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
+++ Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 
 public class TitleButton : MonoBehaviour
 {
+    [SerializeField] Button button;
     Button GameStart;
     Button Rule;
     Button Exit;
@@ -16,10 +17,13 @@ public class TitleButton : MonoBehaviour
     void Start()
     {
         // ボタンコンポーネントの取得
-        GameStart = GameObject.Find("/Canvas/GameStart").GetComponent<Button>();
-        Rule = GameObject.Find("/Canvas/Rule").GetComponent<Button>();
-        Exit = GameObject.Find("/Canvas/Exit").GetComponent<Button>();
-        GameStart.Select();
+        //GameStart = GameObject.Find("/Canvas/GameStart").GetComponent<Button>();
+        //Rule = GameObject.Find("/Canvas/Rule").GetComponent<Button>();
+        //Exit = GameObject.Find("/Canvas/Exit").GetComponent<Button>();
+        //GameStart.Select();
+
+        button = button.GetComponent<Button>();
+        button.Select();
     }
 
     // Update is called once per frame
@@ -30,7 +34,7 @@ public class TitleButton : MonoBehaviour
 
     public void OnClickStart() {
         // 全てのフォーカスを解除
-        EventSystem.current.SetSelectedGameObject(null);
+        //EventSystem.current.SetSelectedGameObject(null);
 
         // ゲームシーン
         SceneManager.LoadScene("Game");
@@ -38,12 +42,12 @@ public class TitleButton : MonoBehaviour
 
     public void OnClickRule() {
         // 全てのフォーカスを解除
-        EventSystem.current.SetSelectedGameObject(null);
+        //EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void OnClickExit() {
         // 全てのフォーカスを解除
-        EventSystem.current.SetSelectedGameObject(null);
+        //EventSystem.current.SetSelectedGameObject(null);
 
         // ゲームを終了する
         UnityEditor.EditorApplication.isPlaying = false;
