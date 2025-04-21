6b9471c85f198740137c99cd36b3b82f60b98ae9
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-04-21 11:53:31 +0900

    TitleComplert

--- Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
+++ Assets/WorkSpace/Kuu/Scripts/TitleButton.cs
@@ -8,22 +8,33 @@ using UnityEngine.UI;
 
 public class TitleButton : MonoBehaviour
 {
-    [SerializeField] Button button;
+    [SerializeField] Canvas RuleCanvas;
+    [SerializeField] Button OK;
     Button GameStart;
     Button Rule;
     Button Exit;
+    //Button OK;
+    //Canvas RuleCanvas;
+    Canvas SelectCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        // ボタンコンポーネントの取得
-        //GameStart = GameObject.Find("/Canvas/GameStart").GetComponent<Button>();
-        //Rule = GameObject.Find("/Canvas/Rule").GetComponent<Button>();
-        //Exit = GameObject.Find("/Canvas/Exit").GetComponent<Button>();
-        //GameStart.Select();
+        // コンポーネントの取得
+        GameStart = GameObject.Find("/SelectCanvas/GameStart").GetComponent<Button>();
+        Rule = GameObject.Find("/SelectCanvas/Rule").GetComponent<Button>();
+        Exit = GameObject.Find("/SelectCanvas/Exit").GetComponent<Button>();
+        OK = OK.GetComponent<Button>();
 
-        button = button.GetComponent<Button>();
-        button.Select();
+        RuleCanvas = RuleCanvas.GetComponent<Canvas>();
+        SelectCanvas = GameObject.Find("/SelectCanvas").GetComponent<Canvas>();
+        // GameStartボタンにフォーカス
+        GameStart.Select();
+
+        // RuleCanvasを非表示
+        RuleCanvas.transform.gameObject.SetActive(false);
+        //button = button.GetComponent<Button>();
+        //button.Select();
     }
 
     // Update is called once per frame
@@ -34,7 +45,7 @@ public class TitleButton : MonoBehaviour
 
     public void OnClickStart() {
         // 全てのフォーカスを解除
-        //EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(null);
 
         // ゲームシーン
         SceneManager.LoadScene("Game");
@@ -42,15 +53,34 @@ public class TitleButton : MonoBehaviour
 
     public void OnClickRule() {
         // 全てのフォーカスを解除
-        //EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // RuleCanvasを表示
+        RuleCanvas.transform.gameObject.SetActive(true);
+        // OKにフォーカスする
+        OK.Select();
+        // SelectCanvasを非表示
+        SelectCanvas.transform.gameObject.SetActive(false);
     }
 
     public void OnClickExit() {
         // 全てのフォーカスを解除
-        //EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(null);
 
         // ゲームを終了する
         UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
+    }
+
+    public void OnClickOk() {
+        // 全てのフォーカスを解除
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // SelectCanvasを表示
+        SelectCanvas.transform.gameObject.SetActive(true);
+        // GameStartにフォーカスする
+        GameStart.Select();
+        // RuleCanvasを非表示
+        RuleCanvas.transform.gameObject.SetActive(false);
     }
 }
