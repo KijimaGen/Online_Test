b506306021381d744e43953ea6f8959f97285355
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-04-30 11:34:17 +0900

    stantochuu

--- Assets/WorkSpace/Kuu/Scripts/StanManager.cs
+++ Assets/WorkSpace/Kuu/Scripts/StanManager.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 
 public class StanManager : MonoBehaviour
 {
-    //GameObject player;
+    // スタンのフラグ
+    bool stanflag;
+    // スタン時間
+    float waitTime;
     // Start is called before the first frame update
     void Start()
     {
         //var player = GetComponent<Player>();
         //player.enabled = false;
+        stanflag = false;
+        waitTime = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (stanflag = true)
+        {
+            //await UniTask.Delay(TimeSpan.FromSeconds(waitTime));
+            stanflag = false;
+        }
     }
 
     public void OnTriggerStay(Collider collider) {
@@ -26,7 +35,7 @@ public class StanManager : MonoBehaviour
             if (Input.GetKey(KeyCode.Space)/* || Input.GetKey("joystick " + index + " button 1")*/) {
                 //chargeSlider.fillAmount += 0.005f;
                 Debug.Log("atack");
-                
+                stanflag = true;
             }
         }
     }
