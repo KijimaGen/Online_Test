cfdb113fcce24262ab92357e78a79c16215e6670
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-05-12 15:13:27 +0900

    Effect

--- Assets/WorkSpace/Kuu/Scripts/shatleEffect.cs
+++ Assets/WorkSpace/Kuu/Scripts/shatleEffect.cs
@@ -12,6 +12,10 @@ public class shatleEffect : MonoBehaviour
     [Tooltip("発生させるエフェクト(パーティクル)")]
     public ParticleSystem particle2;
 
+    [SerializeField]
+    [Tooltip("発生させるエフェクト(パーティクル)")]
+    public ParticleSystem particle3;
+
     void Update()
     {
         /*
@@ -37,7 +41,7 @@ public class shatleEffect : MonoBehaviour
     /// </summary>
     /// <param name="collision"></param>
     public void OnCollisionEnter(Collision collision) {
-        // 当たった相手がcourtだったら
+        // 当たった相手が白床or赤床だったら
         if (collision.gameObject.name == "白床" || collision.gameObject.name == "赤床") {
             // パーティクルシステムのインスタンスを生成する。
             ParticleSystem newParticle = Instantiate(particle1);
@@ -45,21 +49,33 @@ public class shatleEffect : MonoBehaviour
             newParticle.transform.position = this.transform.position;
             // パーティクルを発生させる。
             newParticle.Play();
-            // インスタンス化したパーティクルシステムのGameObjectを5秒後に削除する。(任意)
+            // インスタンス化したパーティクルシステムのGameObjectを3秒後に削除する。
             // ※第一引数をnewParticleだけにするとコンポーネントしか削除されない。
             Destroy(newParticle.gameObject, 3.0f);
+        }
+        // アウトラインに当たったら
+        if (collision.gameObject.name == "outCourt") {
+            // パーティクルシステムのインスタンスを生成する。
+            ParticleSystem newParticle = Instantiate(particle2);
+            // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
+            newParticle.transform.position = this.transform.position;
+            // パーティクルを発生させる。
+            newParticle.Play();
+            // インスタンス化したパーティクルシステムのGameObjectを1.5秒後に削除する。
+            // ※第一引数をnewParticleだけにするとコンポーネントしか削除されない。
+            Destroy(newParticle.gameObject, 2.0f);
         }
     }
     
     public void ShotEffect() {
         // パーティクルシステムのインスタンスを生成する。
-        ParticleSystem newParticle = Instantiate(particle2);
+        ParticleSystem newParticle = Instantiate(particle3);
         // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
         newParticle.transform.position = this.transform.position;
         // パーティクルを発生させる。
         newParticle.Play();
-        // インスタンス化したパーティクルシステムのGameObjectを5秒後に削除する。(任意)
+        // インスタンス化したパーティクルシステムのGameObjectを1秒後に削除する。
         // ※第一引数をnewParticleだけにするとコンポーネントしか削除されない。
-        Destroy(newParticle.gameObject, 3.0f);
+        Destroy(newParticle.gameObject, 1f);
     }
 }
