6114b9c4c43349e40222691010510ec91981bffa
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-05-26 09:36:16 +0900

    kankeinai

--- Assets/WorkSpace/Kuu/Scripts/ChutrialText.cs
+++ Assets/WorkSpace/Kuu/Scripts/ChutrialText.cs
@@ -1,50 +1,85 @@
+using Steamworks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChutrialText : MonoBehaviour
 {
     public TextMesh text;
     // テキストを変えるためのカウント
-    int TextCount = 0;
+    int textCount = 0;
+    // テキストが戻ったことがあるかのフラグ
+    bool textJoin = false;
     // Start is called before the first frame update
     void Start()
     {
-        TextCount = 0;
+        textCount = 0;
+        textJoin = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 下降制限0
+        if (textCount < 0) {
+            textCount = 0;
+        }
+        // 上昇制限8
+        if (textCount > 8) {
+            textCount = 8;
+        }
         // Aを押したらテキストを進める
-        if (Input.GetKeyDown(KeyCode.JoystickButton0) == true || Input.GetMouseButtonDown(0)) {
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) == true || Input.GetMouseButtonDown(0)) {
             // 参加していなかったら進まない
-            if (!(TextCount == 1)) {
-            TextCount++;
+            if (!(textCount == 1) && !textJoin) {
+                textCount++;
+            } else if(textJoin) {
+                textCount++;
             }
         }
         // 一言目でLBを押したら二言目を飛ばす
         if (Input.GetKeyDown(KeyCode.JoystickButton4)) {
-            if (TextCount == 0) {
-                TextCount = 2;
+            if (textCount == 0) {
+                textCount = 2;
+                textJoin = true;
                 // 順当に進む
-            } else if (TextCount == 1) {
-                TextCount++;
+            } else if (textCount == 1) {
+                textCount++;
+                textJoin = true;
             }
 
         }
+        // Bを押したらテキストを戻す
+        if (Input.GetKeyDown(KeyCode.JoystickButton2) == true || Input.GetMouseButtonDown(1)) {
+            textCount--;
+        }
 
-        if (TextCount == 0) {
+        if (textCount == 0) {
             text.text = "ここはチュートリアルルームです";
-        } else if (TextCount == 1) {
+        } else if (textCount == 1) {
             text.text = "LBを押してゲームに参加しましょう";
-        } else if (TextCount == 2) {
+        } else if (textCount == 2) {
             text.text = "最大4人まで参加できます";
-        } else if (TextCount == 3) {
+        } else if (textCount == 3) {
             text.text = "赤と白の床を踏むとそれぞれのチームになります";
-        } else if (TextCount == 4) {
+        } else if (textCount == 4) {
+            text.text = "Bを押すとラケットを振れます";
+        } else if (textCount == 5) {
+            text.text = "チャージがMAXになるとジャンプスマッシュが打てます";
+        } else if (textCount == 6) {
+            text.text = "Bで相手プレイヤーを攻撃すると吹っ飛びます";
+        } else if (textCount == 7) {
+            text.text = "倒れてしまった時はAを押せば戻ります";
+        } else if (textCount == 8) {
+            text.text = "STARTボタンで試合開始です";
+        }
 
+        // BACKボタンでタイトルへ戻る
+        if (Input.GetKeyDown(KeyCode.JoystickButton6)) {
+            // タイトルへ
+            SceneManager.LoadScene("Title");
         }
     }
 }
