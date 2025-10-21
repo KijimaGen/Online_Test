0149588405ee6d99915cb63c01526fe77a3c6457
ichikawakuu <mcd2476004@stu.o-hara.ac.jp>
2025-10-22 01:51:52 +0900

    End project
    Update TutorialScene

--- Assets/WorkSpace/Kuu/Scripts/TitleEbiUIManager.cs
+++ Assets/WorkSpace/Kuu/Scripts/TitleEbiUIManager.cs
@@ -8,13 +8,19 @@ using static GameManager;
 
 public class TitleEbiUIManager : MonoBehaviour
 {
+    // セレクトキャンバス
     [SerializeField] Canvas SelectCanvas;
+    // イベントシステム
     [SerializeField] EventSystem eventSystem;
+    // それぞれのエビUI
     [SerializeField] Image startEbi;
     [SerializeField] Image ruleEbi;
     [SerializeField] Image exitEbi;
     [SerializeField] Image okEbi;
     [SerializeField] Image tutorialEbi;
+
+    // セレクトしているオブジェクト
+    GameObject selectObject;
 
     // Start is called before the first frame update
     void Start()
@@ -24,41 +30,65 @@ public class TitleEbiUIManager : MonoBehaviour
         exitEbi.transform.gameObject.SetActive(false);
         okEbi.transform.gameObject.SetActive(false);
         tutorialEbi.transform.gameObject.SetActive(false);
+
+        selectObject = eventSystem.currentSelectedGameObject.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (eventSystem.currentSelectedGameObject.gameObject.name == "GameStart") {
-            startEbi.transform.gameObject.SetActive(true);
+        // nullチェック
+        if (selectObject != null)
+        {
+            // 現在セレクトされているのが
+            // GameStart
+            if (selectObject.name == "GameStart")
+            {
+                startEbi.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                startEbi.transform.gameObject.SetActive(false);
+            }
+            // Rule
+            if (selectObject.name == "Rule")
+            {
+                ruleEbi.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                ruleEbi.transform.gameObject.SetActive(false);
+            }
+            // Exit
+            if (selectObject.name == "Exit")
+            {
+                exitEbi.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                exitEbi.transform.gameObject.SetActive(false);
+            }
+            // OK
+            if (selectObject.name == "OK")
+            {
+                okEbi.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                okEbi.transform.gameObject.SetActive(false);
+            }
+            // Tutorial
+            if (selectObject.name == "Tutorial")
+            {
+                tutorialEbi.transform.gameObject.SetActive(true);
+            }
+            else
+            {
+                tutorialEbi.transform.gameObject.SetActive(false);
+            }
+        } else
+        {
+            return;
         }
-        else {
-            startEbi.transform.gameObject.SetActive(false); 
-        }
-        if (eventSystem.currentSelectedGameObject.gameObject.name == "Rule") {
-            ruleEbi.transform.gameObject.SetActive(true);
-        }
-        else {
-            ruleEbi.transform.gameObject.SetActive(false);
-        }
-        if (eventSystem.currentSelectedGameObject.gameObject.name == "Exit") {
-            exitEbi.transform.gameObject.SetActive(true);
-        }
-        else {
-            exitEbi.transform.gameObject.SetActive(false);
-        }
-        if (eventSystem.currentSelectedGameObject.gameObject.name == "OK") {
-            okEbi.transform.gameObject.SetActive(true);
-        }
-        else {
-            okEbi.transform.gameObject.SetActive(false);
-        }
-        if (eventSystem.currentSelectedGameObject.gameObject.name == "Tutorial") {
-            tutorialEbi.transform.gameObject.SetActive(true);
-        }
-        else {
-            tutorialEbi.transform.gameObject.SetActive(false);
-        }
-        
     }
 }
